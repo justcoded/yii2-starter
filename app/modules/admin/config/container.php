04553e28f7ae1d9e4d5118325665068d2bd93375
Alex Prokopenko <ab.prokopenko@gmail.com>
2019-07-13 15:58:30 +0300

    Configured RBAC module to work with bootstrap4

--- app/modules/admin/config/container.php
+++ app/modules/admin/config/container.php
@@ -6,5 +6,11 @@ return [
 				'style' => 'width:10px;',
 			],
 		],
+		'justcoded\yii2\rbac\widgets\RbacGridView' => [
+			'class' => \app\modules\admin\widgets\BoxGridView::class,
+		],
+		'justcoded\yii2\rbac\widgets\RbacActiveForm' => [
+			'class' => \yii\bootstrap4\ActiveForm::class,
+		],
 	],
 ];
